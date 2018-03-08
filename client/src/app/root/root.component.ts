e633750882e63edb9bf491e6d5dcdf94316cc248
kre-cmi <remo.kessler@cmiag.ch>
2018-03-08 09:00:13 +0100

    Added Client dependencies

---
+++ client/src/app/root/root.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './root.component.html',
+  styleUrls: ['./root.component.less']
+})
+export class RootComponent {
+}
