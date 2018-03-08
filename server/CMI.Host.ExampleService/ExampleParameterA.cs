341e2b6ce0f0c3fb21989de1a0473f6085826939
kre-cmi <remo.kessler@cmiag.ch>
2018-03-08 08:20:27 +0100

    Folder structure for client and server created and linked

---
+++ server/CMI.Host.ExampleService/ExampleParameterA.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMI.Host.ExampleServiceA
+{
+    public class ExampleParameterA
+    {
+        public string MailAdress { get; set; }
+    }
+}
---
+++ server/CMI.Host.ExampleService/ExampleServiceA.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMI.Host.ExampleServiceA
+{
+    public class ExampleServiceA
+    {
+        public void Start()
+        {
+            // ExampleParameterA
+        }
+
+        public void Stop()
+        {
+            
+        }
+    }
+}
